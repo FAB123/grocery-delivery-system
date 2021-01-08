a584887b541c3ca55c3b7b3ff51dff35bca597e1
Fysal KT <fysalkt@gmail.com>
2021-01-08 15:49:27 +0300

    PWA added

--- public/sworker.js
+++ public/sworker.js
@@ -7,7 +7,7 @@ self.addEventListener("install", e=>{
 });
 
 self.addEventListener("fetch", e=>{
-    e.respondwith(
+    e.respondWith(
         caches.match(e.request).then(response=>{
             return response || fetch(e.request)
         })
