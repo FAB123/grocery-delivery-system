971aacfb9881ecf2a4ef5b799780e157a9ba82d2
Fysal KT <fysalkt@gmail.com>
2021-01-16 01:12:40 +0300

    repayment Facility fixed

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,26 @@
-const mongoClient=require('mongodb').MongoClient
-const state={
-    db:null
+const mongoClient = require('mongodb').MongoClient
+const state = {
+    db: null
 }
 
 const onlineUrl = 'mongodb+srv://fysalkt:Faika123.@cluster0.yqgca.mongodb.net/grocery_delivery_system?retryWrites=true&w=majority'
 
-module.exports.connect=function(done){
-    const url='mongodb://localhost:27017'
-    const dbname='grocery_delivery_system'
+module.exports.connect = function (done) {
+    const url = 'mongodb://localhost:27017'
+    const dbname = 'grocery_delivery_system'
 
-    mongoClient.connect(url,{useUnifiedTopology: true},(err,data)=>{
-   // mongoClient.connect(onlineUrl,{useUnifiedTopology: true},(err,data)=>{
-        if(err){
+    //mongoClient.connect(url,{useUnifiedTopology: true},(err,data)=>{
+    mongoClient.connect(onlineUrl, { useUnifiedTopology: true }, (err, data) => {
+        if (err) {
             return done(err)
         }
-        else{
-            state.db=data.db(dbname)
+        else {
+            state.db = data.db(dbname)
             done()
         }
     })
 }
 
-module.exports.get=function(){
+module.exports.get = function () {
     return state.db
 }
