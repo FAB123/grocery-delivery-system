f5930d987de138d361c2189c81fafc6bb2db6556
Fysal KT <fysalkt@gmail.com>
2021-01-21 03:18:11 +0300

    Favorite Icon Added

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,14 @@ const state = {
     db: null
 }
 
-
+const onlineUrl = 'mongodb+srv://fysalkt:Faika123.@cluster0.yqgca.mongodb.net/grocery_delivery_system?retryWrites=true&w=majority'
 
 module.exports.connect = function (done) {
     const url = 'mongodb://localhost:27017'
     const dbname = 'grocery_delivery_system'
 
-    //mongoClient.connect(url,{useUnifiedTopology: true},(err,data)=>{
-    mongoClient.connect(onlineUrl, { useUnifiedTopology: true }, (err, data) => {
+    mongoClient.connect(url,{useUnifiedTopology: true},(err,data)=>{
+    //mongoClient.connect(onlineUrl, { useUnifiedTopology: true }, (err, data) => {
         if (err) {
             return done(err)
         }
